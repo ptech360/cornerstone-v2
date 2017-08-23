d43d41ed1b531c016d88a0794f9fb2739d567946
Abhay2012 <abhay.sehgal2012@gmail.com>
2017-08-23 15:34:52 +0530

    appreciation,complaint and suggestion module

---
+++ src/app/component/appreciation/appreciation.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core'; 
+import { AppreciationComponent } from './appreciation.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared.module';
+import { ByMeComponent } from './by-me/byme';
+import { ForMeComponent } from './for-me/forme';
+import { AddAppreciation } from './add/add';
+
+@NgModule({
+	imports : [ SharedModule, RouterModule.forChild([
+			{
+				path : '',
+				component : AppreciationComponent,
+				children : [
+					{
+						path : 'for-student',
+						component : ByMeComponent
+					},
+					{
+						path : 'for-me',
+						component : ForMeComponent
+					}
+				]
+			},
+			{
+				path : 'add-appreciation',
+				component : AddAppreciation
+			}
+			
+		])],
+	declarations : [ AddAppreciation, ByMeComponent, AppreciationComponent, ForMeComponent ]
+}) 
+export class AppreciationModule {
+	
+}
