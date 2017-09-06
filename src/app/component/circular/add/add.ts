5e792961ec6ab84d68b8f04cc710ea22a278d4dd
Abhay2012 <abhay.sehgal2012@gmail.com>
2017-09-06 22:54:11 +0530

    error resolved

--- src/app/component/circular/add/add.ts
+++ src/app/component/circular/add/add.ts
@@ -26,7 +26,7 @@ export class AddCircular implements OnInit, AfterViewInit {
   public submitProgress: boolean = false;
   public standardLoader:boolean=false;
   public audienceLoader:boolean=false;
-  
+  public auditype : any = -1;
   constructor(private circserv: CircularService,
     private commonService: CommonService,
     private _location: Location,
@@ -117,6 +117,7 @@ export class AddCircular implements OnInit, AfterViewInit {
   public buildCircularData(circular: any) {
 
     this.circularType = circular;
+    this.circularType.splice(0,0,{ id : -1, name : 'Select Audience' });
   }
 
   public onCircularType(event: any) {
